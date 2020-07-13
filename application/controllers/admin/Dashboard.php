3d8450a2bdd5a0b10ab423f7af2031d5f76e1da9
Winarto <winarto007zing@gmail.com>
2020-07-13 09:49:33 +0700

    Update Dashboard.php

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -1,13 +1,14 @@
 <?php
-
-class Dashboard extends CI_Controller 
-{
-    public function index()
+    defined('BASEPATH') OR exit('No direct script access allowed');
+    
+    class Dashboard extends CI_Controller 
     {
-        $this->load->view('templates/admin/header.php');
-        $this->load->view('templates/admin/sidebar.php');
-        $this->load->view('admin/dashboard.php');
-        $this->load->view('templates/admin/footer.php');
-    }
+        public function index()
+        {
+            $this->load->view('templates/admin/header.php');
+            $this->load->view('templates/admin/sidebar.php');
+            $this->load->view('admin/dashboard.php');
+            $this->load->view('templates/admin/footer.php');
+        }
 
-}
+    }
