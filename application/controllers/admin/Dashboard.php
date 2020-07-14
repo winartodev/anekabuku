0f8096c8e474aa77a22759418ed9c9084bdc8ed5
Winarto <winarto007zing@gmail.com>
2020-07-14 14:30:22 +0700

    Update Dashboard.php

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -3,11 +3,18 @@
     
     class Dashboard extends CI_Controller 
     {
+        public function __construct()
+        {
+            parent::__construct();		
+            $this->load->model('model_buku');
+        }
+
         public function index()
         {
+            $data['buku'] = $this->model_buku->count_buku();
             $this->load->view('templates/admin/header.php');
             $this->load->view('templates/admin/sidebar.php');
-            $this->load->view('admin/dashboard.php');
+            $this->load->view('admin/dashboard.php', $data);
             $this->load->view('templates/admin/footer.php');
         }
 
