fe04d8411cfd69e0323e7553d016e82cde96f890
Winarto <winarto007zing@gmail.com>
2020-07-14 07:08:42 +0700

    Update Penerbit.php

--- application/controllers/admin/Penerbit.php
+++ application/controllers/admin/Penerbit.php
@@ -3,7 +3,8 @@
 
     class Penerbit extends CI_Controller 
     {
-        public function __construct(){
+        public function __construct()
+        {
             parent::__construct();		
             $this->load->model('model_penerbit');
             $this->load->library('form_validation');
