7bce969b2c8251010cfe2c8a23e7283e05272650
Winarto <winarto007zing@gmail.com>
2020-07-16 08:33:04 +0700

    Create Guest Template

---
+++ application/views/templates/guest/footer.php
@@ -0,0 +1,64 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2020 <a href="<?= base_url('admin/dashboard') ?>">Aneka Buku</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.0.5
+    </div>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="<?= base_url('assets') ?>/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?= base_url('assets') ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- DataTables -->
+<script src="<?= base_url('assets') ?>/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="<?= base_url('assets') ?>/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="<?= base_url('assets') ?>/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="<?= base_url('assets') ?>/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?= base_url('assets') ?>/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?= base_url('assets') ?>/dist/js/demo.js"></script>
+<!-- Select2 -->
+<script src="<?= base_url('assets') ?>/plugins/select2/js/select2.full.min.js"></script>
+<!-- bs-custom-file-input -->
+<script src="<?= base_url('assets') ?>/plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
+<!-- page script -->
+<script>
+  $(function () {
+  	$("#example1").DataTable({
+  		"responsive": true,
+  		"autoWidth": false,
+  	});
+  	$('#example2').DataTable({
+  		"paging": true,
+  		"lengthChange": false,
+  		"searching": false,
+  		"ordering": true,
+  		"info": true,
+  		"autoWidth": false,
+  		"responsive": true,
+  	});
+  });
+  //Initialize Select2 Elements
+  $('.select2').select2()
+
+  //Initialize Select2 Elements
+  $('.select2bs4').select2({
+  	theme: 'bootstrap4'
+  });
+
+  $(document).ready(function () {
+  bsCustomFileInput.init();
+});
+</script>
+</body>
+</html>
