562f678cce43032ec9752c0c7e1f13807915868e
Winarto <winarto007zing@gmail.com>
2020-07-16 10:41:41 +0700

    Update footer.php

--- application/views/templates/guest/footer.php
+++ application/views/templates/guest/footer.php
@@ -14,8 +14,6 @@
 </div>
 <!-- ./wrapper -->
 
-<!-- jQuery -->
-<script src="<?= base_url('assets') ?>/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
 <script src="<?= base_url('assets') ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- DataTables -->
