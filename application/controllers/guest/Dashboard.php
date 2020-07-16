017aafeb4e631826edf5af4865f3a93b2f3dfe07
Winarto <winarto007zing@gmail.com>
2020-07-16 08:36:43 +0700

    Create Guest Dashboard

--- application/controllers/guest/Dashboard.php
+++ application/controllers/guest/Dashboard.php
@@ -20,21 +20,5 @@
             $this->load->view('guest/dashboard.php', $data);
             $this->load->view('templates/guest/footer.php');
         }
-
-        public function desc_limiter($where_description) 
-        {
-            $string = strip_tags($where_description);
-            if (strlen($string) > 500) {
-            
-                // truncate string
-                $stringCut = substr($string, 0, 500);
-                $endPoint = strrpos($stringCut, ' ');
-            
-                //if the string doesn't contain any space then it will cut without word basis.
-                $string = $endPoint? substr($stringCut, 0, $endPoint) : substr($stringCut, 0);
-                $string .= '... <a href="/this/story">Read More</a>';
-            }
-        }
-
     }
     
