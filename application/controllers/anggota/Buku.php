f1a4aa08d8ef1dfa66a0343c55d22797fc2a5d78
Winarto <winarto007zing@gmail.com>
2020-07-17 13:15:43 +0700

    Modified Controller in Anggota

--- application/controllers/anggota/Buku.php
+++ application/controllers/anggota/Buku.php
@@ -5,7 +5,19 @@
     {
         public function __construct()
         {
-            parent::__construct();		
+            parent::__construct();	
+            
+            if ($this->session->userdata('role_id') != '2') 
+            {
+                $this->session->set_flashdata('pesan','<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                                        <b>Anda Belum Login.</b>
+                                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>');
+                redirect(base_url('guest/login'));
+            }
+
             $this->load->model('model_buku');
             $this->load->model('model_kategori');
         }
