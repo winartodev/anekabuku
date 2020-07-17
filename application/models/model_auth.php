e99fca433591bef3366dda90dd1ef0ff150b8049
Winarto <winarto007zing@gmail.com>
2020-07-17 13:06:50 +0700

    Update model_auth.php

--- application/models/model_auth.php
+++ application/models/model_auth.php
@@ -1,11 +1,13 @@
 <?php
 class Model_Auth extends CI_Model {
-    function auth_anggota ($username,$password){
+    function auth_anggota ($username,$password)
+    {
         $query=$this->db->query("SELECT * FROM tbl_anggota WHERE id_anggota='$username' AND password=MD5('$password') LIMIT 1");
         return $query;
     }
     
-    function auth_petugas($username,$password){
+    function auth_petugas($username,$password)
+    {
         $query=$this->db->query("SELECT * FROM tbl_petugas WHERE id_petugas='$username' AND password=MD5('$password') LIMIT 1");
         return $query;
     }
