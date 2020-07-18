e9de26e78914888504419fdaa1b473adc47482ce
Winarto <winarto007zing@gmail.com>
2020-07-18 11:26:44 +0700

    Update dashboard.php

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -57,7 +57,7 @@
             <!-- small box -->
             <div class="small-box bg-warning">
               <div class="inner">
-                <h3>44</h3>
+                <h3><?= $peminjaman ?></h3>
 
                 <p>Jumlah Peminjaman</p>
               </div>
